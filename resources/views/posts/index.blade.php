7ccf00f4f65d130817d54f32944c85022b4ba321
rskb0126 <dai.0120.ki@gmail.com>
2022-08-19 16:50:58 +0900

    [add]記事一覧画面

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        {{--記事一覧のクラスを作成--}}
+        <div class = 'posts'>
+            @foreach ($posts as $post)
+                {{--記事一つ一つのクラスを作成--}}
+                <div class = 'post'>
+                    <h2 class = 'title'>id={{ $post->id }} {{ $post->title }}</h2>
+                    <p class = 'body'>{{ $post->body }}</p>
+                    <p class ='body'>この文章は{{ $post->created_at }}に作成された。</p>
+                </div>
+            @endforeach
+        </div>
+    </body>
+</html>
