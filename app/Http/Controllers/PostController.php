e52390ce4280f2e7160c50c3a651b34bb913cc66
rskb0126 <dai.0120.ki@gmail.com>
2022-08-22 18:41:17 +0900

    [add]投稿の編集機能

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,7 +20,7 @@ class PostController extends Controller
          * データベースから受け取ったデータをviewに渡す
          * postsにはpost1,post2,....のようにデータが渡される
          */
-        return view('posts/index')->with(['posts' => $post->getpaginateByLimit(2)]);
+        return view('posts/index')->with(['posts' => $post->getpaginateByLimit()]);
     }
     
     public function show(Post $post)
@@ -35,9 +35,25 @@ class PostController extends Controller
 
     public function store(PostRequest $request, Post $post)
     {
+        // create.blade.phpでnameに定義されているpostを参照している
         $input = $request['post'];
         // fillした後にsaveでSQLのinsertと同じクエリになる
         $post->fill($input)->save();
+        // .は文字列をつなげる（/posts/{id} という構造になっている)
         return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('posts/edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, Post $post)
+    {
+        // create.blade.phpでnameに定義されているpostを参照している
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+        return redirect('/posts/' . $post->id);
+        
     }
 }
